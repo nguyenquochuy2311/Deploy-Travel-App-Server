7941e56e4e1673a3e083a27f1b65ffecac7fb83e
Huy Nguyen <nguyenquochuyhbt@gmail.com>
2022-06-18 14:37:28 +0700

    update deploy

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ apiRouters(app);
 //     })
 // });
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started: http://localhost:${port}`)
