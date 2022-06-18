fa328812648f6a75c843fc41e8ec443fbfb4de80
Huy Nguyen <nguyenquochuyhbt@gmail.com>
2022-06-18 11:56:25 +0700

    deploy

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,7 +5,7 @@ import addressRouter from './addressesRouter.js';
 import addressDetailRouter from './addressesDetailRouter.js';
 import countryRouter from './countryRouter.js';
 import serviceRouter from './serviceRouter.js';
-import refBookingOutcomeRouter from './refBookingOutcomeRouter.js';
+import refBookingOutcomeRouter from './refBookingOutComeRouter.js';
 import refBookingStatusPaymentRouter from './refBookingStatusPaymentRouter.js';
 import bookingRouter from './bookingRouter.js';
 import restaurantsRouter from './restaurantsRouter.js';
